60589a52700e94166d30826c4b4fc7701ca4f29c
cicaglobal <info@cicaglobal.com.au>
2025-04-28 13:38:00 +0800

    Update rosters.js

--- api/rosters.js
+++ api/rosters.js
@@ -1,5 +1,7 @@
+import fetch from 'node-fetch';
+
 export default async function handler(req, res) {
-  const token = process.env.PCO_TOKEN; // pulls from your environment variable
+  const token = process.env.PCO_TOKEN; // pulls from your environment variables
   
   const response = await fetch('https://api.planningcenteronline.com/services/v2/plans?order=sort_date', {
     headers: {

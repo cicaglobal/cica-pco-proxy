43ed7d12dd4fb1e3dd6f7b34fc5d7d27e417c701
cicaglobal <info@cicaglobal.com.au>
2025-04-28 13:31:37 +0800

    Create rosters.js

---
+++ api/rosters.js
@@ -0,0 +1,14 @@
+export default async function handler(req, res) {
+  const token = process.env.PCO_TOKEN; // pulls from your environment variable
+  
+  const response = await fetch('https://api.planningcenteronline.com/services/v2/plans?order=sort_date', {
+    headers: {
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json'
+    }
+  });
+
+  const data = await response.json();
+
+  res.status(200).json(data);
+}
